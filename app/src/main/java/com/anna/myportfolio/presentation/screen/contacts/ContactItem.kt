4ddae43a7c6f5d1316eef8f283a702c67d8eff34
anna <rigariga@inbox.ru>
2025-08-06 22:35:11 +0300

    Added a new parameter to the fun ContactItem

--- app/src/main/java/com/anna/myportfolio/presentation/screen/contacts/ContactItem.kt
+++ app/src/main/java/com/anna/myportfolio/presentation/screen/contacts/ContactItem.kt
@@ -1,19 +1,38 @@
 package com.anna.myportfolio.presentation.screen.contacts
 
+import androidx.annotation.DrawableRes
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import com.anna.myportfolio.R
 
 @Composable
-fun ContactItem(title: String, value: String?) {
+fun ContactItem(
+    title: String,
+    value: String?,
+    @DrawableRes iconRes: Int = R.drawable.ic_vk// лежит в папке res drawable дефолтное значение потом удалить
+) {
     Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
-        Text(text = title, fontWeight = FontWeight.Bold, modifier = Modifier.padding(end = 16.dp)) // Отступ справа
+        Text(
+            text = title,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(end = 16.dp)
+        ) // Отступ справа
+        Icon(
+            painter = painterResource(iconRes),
+            contentDescription = "Иконка", // добавить в ресурсы и добавить перевод
+            tint = Color.Red // меняет цвет
+            // modifier подбирает размер
+        )
         val editedValue = value ?: "-"
         Text(text = editedValue)
     }
